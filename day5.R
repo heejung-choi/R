c2af8091caab3b000f0e643a81a660eea75d351f
heejung-choi <choi951008@naver.com>
2020-03-26 10:34:29 +0900

    20200326.R.STUDY

---
+++ day5.R
@@ -0,0 +1,68 @@
+# 교재 81페이지
+library()
+installed.packages()
+search()
+read_excel()
+install.packages("readxl")
+library(readxl) # require(readxl)
+excel_data_ex <- read_excel("book/data_ex.xls")
+getwd()
+View(excel_data_ex)
+search()
+
+# 웹 크롤링과 스크래핑
+
+install.packages("rvest") 
+library(rvest)
+
+url <- "http://unico2013.dothome.co.kr/crawling/tagstyle.html"
+text <- read_html(url)
+text
+
+nodes <- html_nodes(text, "div")
+nodes
+title <- html_text(nodes)
+title
+
+node1 <- html_nodes(text, "div:nth-of-type(1)")
+node1
+html_text(node1)
+html_attr(node1, "style")
+
+node2 <- html_nodes(text, "div:nth-of-type(2)")
+node2
+html_text(node2)
+html_attr(node2, "style")
+
+node3 <- html_nodes(text, "div:nth-of-type(3)")
+node3
+html_text(node3)
+
+
+# 단일 페이지(rvest 패키지 사용)
+install.packages("rvest"); 
+library(rvest)
+text<- NULL
+url<- "http://movie.naver.com/movie/point/af/list.nhn?page=1"
+text <- read_html(url,  encoding="CP949")
+text
+# 영화제목
+nodes <- html_nodes(text, ".movie")
+title <- html_text(nodes)
+title
+# 영화평점
+nodes <- html_nodes(text, ".title em")
+point <- html_text(nodes)
+point
+# 영화리뷰 
+nodes <- html_nodes(text, xpath="//*[@id='old_content']/table/tbody/tr/td[2]/text()")
+imsi <- html_text(nodes, trim=TRUE)
+review <- imsi[nchar(imsi) > 0] 
+review
+if(length(review) == 10) {
+  page <- cbind(title, point)
+  page <- cbind(page, review)
+  write.csv(page, "movie_reviews.csv")
+} else {
+  cat("리뷰글이 생략된 데이터가 있네요ㅜㅜ\n")
+}
